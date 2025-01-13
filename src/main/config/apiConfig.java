413c2588de2d170a8bd41147569d9f5214b54a5d
brizzeta <76489220+brizzeta@users.noreply.github.com>
2025-01-13 17:46:15 +0200

    обновила работу нрад юзерами

---
+++ src/main/config/apiConfig.java
@@ -0,0 +1,8 @@
+@Configuration
+public class apiConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
